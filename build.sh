10d8f94927922fd4e96594915d1d59bd61d60117
Kilian Gosewisch <kilian2798@gmail.com>
2021-12-26 02:12:24 +0100

    fix: install and build scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # this is just a convenience script
 # no guarantee that it works!
-PI_DIRECTORY="/run/media/$USER/RPI-RP2"
 pwd
 mkdir build
 cd build &&
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PI_DIRECTORY="/run/media/$USER/RPI-RP2"
 ./build.sh
 stty -F /dev/ttyACM0 1200
 while [ ! -d "$PI_DIRECTORY" ];
@@ -8,4 +9,4 @@ done
 
 sleep 1.5
 
-cp freedeck-pico.uf2 $PI_DIRECTORY
+cp build/freedeck-pico.uf2 $PI_DIRECTORY
